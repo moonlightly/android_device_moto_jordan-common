0d756eb08472e7130d79cbc0ea45d9410c7f3e2e
Kayant <anthonysanwo@googlemail.com>
2012-08-06 19:55:11 +0100

    disable hostap for now until available in jellybean

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
-ifeq ($(TARGET_PRODUCT),$(filter $(TARGET_PRODUCT),aokp_mb525 aokp_jordan_plus))
+ifeq ($(TARGET_PRODUCT),$(filter $(TARGET_PRODUCT), aokp_mb525 aokp_defy_plus))
 $(call inherit-product, vendor/motorola/jordan-common/jordan-vendor.mk)
 endif
 
